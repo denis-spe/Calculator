548fbe2ec105647879b4c49e1840a10748f97961
denisbrian07@gmail.com <denisbrian07@gmail.com>
2022-10-08 14:45:40 +0200

    Fixed the modulus functionality

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// Shortcut the console.log and assert.
+let {log} = console;
+let {assert} = console;
+
 // Instantiate boxInput 
 var boxInput = document.querySelector('#input-box');
 
@@ -23,8 +27,8 @@ document.addEventListener('DOMContentLoaded',
         
         else{
           userInput += this.dataset.num;
-          boxInput.autofocus = true;
-          boxInput.value = userInput;
+          boxInput.focus();
+          boxInput.value = userInput.replace("/100", "%");
         }
         
       }
@@ -34,18 +38,51 @@ document.addEventListener('DOMContentLoaded',
         finalOutPut = "";
         document.querySelector('.result').innerHTML = "";
         boxInput.value = userInput;
+        boxInput.focus();
       }
+      
+      var percentSign = function(output){
+        let _output = [];
+        if (output.includes("%")){
+          const splited = output.split(/([*/+-])/g);
+          
+          // loop through the splited array.
+          for (let val of splited){
+            if (val.includes("%")){
+              val = val.replace("%", "") / 100
+              _output.push(val);
+            }
+            else{
+              _output.push(val)
+            }
+          }
+        }
+        return _output.join("");
+          
+      }
+      // Assertion Test.
+      assert(percentSign("2%") === "0.02");
+      assert(percentSign("5%") === "0.05");
+      assert(percentSign("5%*6") === "0.05*6");
+      // log(percentSign("7%+8%*9-6%"));
+      
       
       signCallback = (sign) => {
         if (btn.value == sign){
           if (finalOutPut){
-            finalOutPut = finalOutPut + sign;
+            
+            finalOutPut = finalOutPut.replace("/100", "%") + sign;
             boxInput.value = finalOutPut;
+            boxInput.focus();
           }
           else{
-            userInput = userInput + sign;
+            userInput = userInput.replace("/100", "%") + sign;
             boxInput.value = userInput;
+            boxInput.focus();
           }
+          /*if (btn.value == "%"){
+            percentSign(boxInput.value);
+          }*/
         }
       }
       
@@ -54,21 +91,29 @@ document.addEventListener('DOMContentLoaded',
       signCallback("-")
       signCallback("÷")
       signCallback("x")
+      signCallback("%")
       
       if (btn.value == "="){
         
         if (finalOutPut){
-          finalOutPut = finalOutPut.replace("÷", "/").replace("x", '*');
-          userInput = eval(finalOutPut);
-          boxInput.value = eval(finalOutPut);
+          finalOutPut = finalOutPut.replace("÷", "/").replace("x", '*').replace("%", "/100");
+          //finalOutPut = percentSign(finalOutPut);
+          //log(finalOutPut);
+          
+          finalOutPut = eval(finalOutPut);
+          
+          userInput = finalOutPut;
+          boxInput.value = finalOutPut;
           finalOutPut = "";
           document.querySelector('.result').innerHTML = "";
         }
         else{
+          userInput = userInput.replace("%", "/100");
           // save the final output
           finalOutPut = eval(userInput);
           // put the final output to the input box
-          boxInput.value = eval(userInput);
+          userInput = eval(userInput);
+          boxInput.value = userInput;
           document.querySelector('.result').innerHTML = "";
           userInput = "";
         }
@@ -76,14 +121,22 @@ document.addEventListener('DOMContentLoaded',
         
       }
       
-      if (userInput.match(/\d+[+-÷x]\d+/g)){
-        userInput = userInput.replace("÷", "/").replace('x', "*");
+      if (userInput.match(/\d+[+-÷x%]\d+/g)){
+        userInput = userInput.replace("÷", "/").replace('x', "*").replace("%", "/100");
+       
+        document.querySelector('.result').innerHTML = eval(userInput);
+      }
+      if (userInput.match(/\d+[%]/g)){
+        // Replace modulus sign to /100.
+        userInput = userInput.replace("%", "/100");
+        
         document.querySelector('.result').innerHTML = eval(userInput);
       }
       
-      if (finalOutPut.match(/\d+[+-÷x]\d+/g)){
+      
+      /*if (finalOutPut.match(/\d+[+-÷x]\d+/g)){
         document.querySelector('.result').innerHTML = eval(finalOutPut);
-      }
+      }*/
     }
  })
 })
